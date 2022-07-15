4181b7a52482c4062d5b74caa2579ba9c8a19ab1
Laurent Laffont <lolgzs@gresille.org>
2022-07-15 15:49:16 +0200

    Setup Bokeh packaging

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 #=================================================
 # COMMON VARIABLES
 #=================================================
+YNH_PHP_VERSION="7.4"
 
-# dependencies used by the app
-pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+pkg_dependencies="php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-gd yaz libyaz5"
 
 #=================================================
 # PERSONAL HELPERS
